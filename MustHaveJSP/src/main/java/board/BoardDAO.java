019e4414d9e79bce69cef23fe3e443c2a321890f
kkjjss2388 <kkjjss2388@gmail.com>
2025-05-09 17:23:25 +0900

    8장 게시판 만들기 까지

---
+++ MustHaveJSP/src/main/java/board/BoardDAO.java
@@ -0,0 +1,203 @@
+package board;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import common.JDBConnect;
+import jakarta.servlet.ServletContext;
+
+public class BoardDAO extends JDBConnect 
+{
+	public BoardDAO(ServletContext application) 
+	{
+		super(application);
+	}
+	
+	public int selectCount(Map<String, Object >map) 
+	{
+		int totalCount  = 0;
+		
+		String query = "SELECT COUNT(*) FROM board";
+		if(map.get("searchWord") != null) 
+		{
+			query += "WHERE " + map.get("searchField") + " "
+				  + " LIKE '%" +map.get("searchWord") + "%'";
+		}
+		
+		try
+		{
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			rs.next();
+			totalCount = rs.getInt(1);
+		} catch (Exception e)
+		{
+			System.out.println("게시물 수를 구하는 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return totalCount;
+	}
+	public List<BoardDTO> selectList(Map<String, Object> map)
+	{
+		List<BoardDTO> bbs = new Vector<BoardDTO>();
+		
+		String query = "SELECT * FROM board ";
+		if (map.get("searchWord") != null)
+		{
+			query += " WHERE " + map.get("searchFireld") + " "
+					+ " LIKE '%" + map.get("searchWord") + "%' ";
+		}
+		query += " ORDER BY num DESC";
+		
+		try
+		{
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+			
+			while (rs.next())
+			{
+				BoardDTO dto = new BoardDTO();
+				
+				dto.setNum(rs.getString("num"));
+				dto.setTitle(rs.getString("title"));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setId(rs.getString("id"));
+				dto.setVisitcount(rs.getString("visitCount"));
+				
+				bbs.add(dto);
+				
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("게시물 조회 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return bbs;
+	}
+	
+	public int insertWrite(BoardDTO dto)
+	{
+		int result = 0;
+		
+		try
+		{
+			String query = "INSERT INTO board ( "
+						 + " num,title,content,id,visitcount) "
+						 + " VALUES ( "
+						 + " seq_board_num.NEXTVAL, ?, ?, ?, 0)";
+			
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, dto.getTitle());
+			psmt.setString(2, dto.getContent());
+			psmt.setString(3, dto.getId());
+			
+			result = psmt.executeUpdate();
+		}
+		catch (Exception e) 
+		{
+			System.out.println("게시물 입력 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public BoardDTO selectView(String num)
+	{
+		BoardDTO dto = new BoardDTO();
+		
+		String query = "SELECT B,*, M.name"
+					 + " FROM member M INNER JOIN board B "
+					 + " ON M.id=B.id "
+					 + " WHERE num=?";
+		
+		try
+		{
+			psmt = con.prepareStatement(query);
+			psmt.setString(q, num);
+			rs = psmt.executeQuery();
+			
+			if (rs.next())
+			{
+				dto.setNum(rs.getString(1));
+				dto.setTitle(rs.getString(2));
+				dto.setContent(rs.getString("content"));
+				dto.setPostdate(rs.getDate("postdate"));
+				dto.setId(rs.getString("id"));
+				dto.setVisitcount(rs.getString("name"));
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("게시물 상세보기 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return dto;
+	}
+	
+	public void updateVisitCount(String num)
+	{
+		String query = "UPDATE board SET "
+					 + " visitcount=visitcount+1 "
+					 + "WHERE num=?";
+		
+		try
+		{
+			psmt = con.prepareStatement(query);
+			psmt.setString(1, num);
+			psmt.executeQuery();
+		}
+		catch (Exception e)
+		{
+			System.out.println("게시물 조회수 증가 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		public int updateEdit(BoardDTO dto) 
+		{
+			
+		}
+		
+		public List<BoardDTO> selecListPage(Map<String, Object> map)
+		{
+			List<BoardDTO> bbs = new ArrayList<BoardDTO>();
+			
+			String query = " SELECT * FROM ( "
+						 + "	SELECT Tb.*, ROWNUM rNum FROM ( "
+						 + "		SELECT * FROM board ";
+			if (map.get("searchWord") != null) 
+					{
+					query += " WHERE " + map.get("searchField)"
+							+ "LIKE '%" + map.get("searchWord") + "%' ";
+			}
+			query += "		ORDER BY num DESC "
+				  + "		)Tb "
+				  + " ) "
+				  + " WHERE rNum BETWEEN ? AND ?";
+			
+			try
+			{
+				psmt = con.prepareStatement(query);
+				psmt.setString(1, map.get("start").toString());
+				psmt.setString(2, map.get("end").toString());
+				rs = psmt.executeQuery();
+			} catch (Exception e)
+			{
+				// TODO: handle exception
+			}
+				
+					}
+		}
+	}
+	
+		
+}
+
+
+
